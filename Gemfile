a35de8d4bbf01599d9bd54bb6ae161252d1e4851
Juan Sanchez <juansanchez3096@sbcglobal.net>
2016-10-16 00:39:09 -0700

    add shoulda-matchers for rspec

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :test do
   gem "rspec-rails"
   gem "factory_girl_rails"
   gem 'ffaker'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:password_confirmation) }
 	it { is_expected.to be valid }
 
+	it { is_expected.to validate_presence_of(:email) }
+	it { is_expected.to validate_uniqueness_of(:email) }
+	it { is_expected.to validate_confirmation_of(:password) }
+	it { is_expected.to allow_value('example@domain.com').for(:email) }
+
+
+
 end
