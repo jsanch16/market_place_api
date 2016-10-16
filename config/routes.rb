eff910b460cd312579fcd18fe71ff4c5800cdbfa
Juan Sanchez <juansanchez3096@sbcglobal.net>
2016-10-15 23:41:09 -0700

    added devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+require 'api_constraints'
 Rails.application.routes.draw do
+  devise_for :users
 	#maps to /api directory in controllers
 	#routes would be matched by /api/controllername
 
 	namespace :api, defaults: { format: :json} do
-		scope module: :v1, constraints: { ApiConstraints.new(version: 1, default: true) } do
+		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 			
 		end
 	end
